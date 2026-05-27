0b2c14264484aef3c257b9fe63669e14b7f5a8b5
陈斌 <1786512286@qq.com>
2026-05-27 17:31:44 +0800

    github page配置

--- demo/src/router/index.ts
+++ demo/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
@@ -140,7 +140,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
