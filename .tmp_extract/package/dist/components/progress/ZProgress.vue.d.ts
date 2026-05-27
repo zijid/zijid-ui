8c3f8e3e12565f724c12bbaca1716d43d5835956
陈斌 <1786512286@qq.com>
2026-05-27 14:44:57 +0800

    chore: 屏蔽 Playwright 浏览器二进制文件，减少仓库体积

--- .tmp_extract/package/dist/components/progress/ZProgress.vue.d.ts
+++
@@ -1,8 +0,0 @@
-type __VLS_Props = {
-    value?: number;
-};
-declare const _default: import('vue').DefineComponent<__VLS_Props, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<__VLS_Props> & Readonly<{}>, {
-    value: number;
-}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {}, any>;
-export default _default;
-//# sourceMappingURL=ZProgress.vue.d.ts.map
